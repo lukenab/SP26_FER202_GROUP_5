91cebd26160e33c33659aae8581eb2d5639530a7
trangttt <trangtttce191027@gmail.com>
2026-03-08 13:12:46 +0700

    TrangTTT_Update banner for Book list page IU

--- SP26_FER202_Group5/src/App.jsx
+++ SP26_FER202_Group5/src/App.jsx
@@ -2,8 +2,7 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
-//import BookListPage from './pages/BookListPage';
-
+import BookListPage from './pages/BookListPage';
 function App() {
 
 
@@ -11,8 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/" index element={<HomePage/>}/>
-          {/* <Route path="/books" element={<BookListPage/>}/> */}
+          <Route index element={<HomePage/>}/>
+          <Route path="/books" element={<BookListPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
