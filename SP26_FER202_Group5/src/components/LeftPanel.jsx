03ba94737ba54d9801ddb67fd937d2dc5e4e5850
trangttt <trangtttce191027@gmail.com>
2026-03-09 21:28:08 +0700

    TrangTTT_re-style components

--- SP26_FER202_Group5/src/components/LeftPanel.jsx
+++ SP26_FER202_Group5/src/components/LeftPanel.jsx
@@ -1,25 +1,21 @@
 import { Card } from "react-bootstrap";
 import MiniBookCard from "./MiniBookCard";
 
-const RightPanel = ({ newBooks }) => {
+const LeftPanel = ({ title, newBooks }) => {
 
   return (
     <Card className="shadow-sm border-0">
-
-      <Card.Header className="fw-bold">
-        New Books
+      <Card.Header className="fw-bold fs-4">
+       {title}
       </Card.Header>
-
       <Card.Body>
-
         {newBooks.map((book) => (
           <MiniBookCard key={book.id} book={book} />
         ))}
-
       </Card.Body>
 
     </Card>
   );
 };
 
-export default RightPanel;
+export default LeftPanel;
