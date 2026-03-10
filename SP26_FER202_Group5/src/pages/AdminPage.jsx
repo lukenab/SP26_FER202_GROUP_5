af4fb67d273a183d0d66eca3707d298c17b8d4ac
lapnh <lapnh.ce19042@gmail.com>
2026-03-10 14:49:48 +0700

    LapNH_Register & Login logic (Auth config)_role-admin-user

---
+++ SP26_FER202_Group5/src/pages/AdminPage.jsx
@@ -0,0 +1,10 @@
+function AdminPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Admin Dashboard</h1>
+      <p>Admin only page</p>
+    </div>
+  );
+}
+
+export default AdminPage;
--- SP26_FER202_Group5/src/pages/Login.jsx
+++ SP26_FER202_Group5/src/pages/Login.jsx
@@ -1,26 +1,44 @@
-import { Link } from "react-router-dom";
-import "./auth.css";
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import './auth.css';
+import axios from 'axios';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.get('http://localhost:5000/users');
+    const users = res.data;
+
+    const user = users.find((u) => u.email === email && u.password === password);
+
+    if (user) {
+      localStorage.setItem('user', JSON.stringify(user));
+
+      if (user.role === 'admin') {
+        navigate('/admin');
+      } else {
+        navigate('/');
+      }
+    } else {
+      alert('Invalid email or password');
+    }
+  };
+
   return (
     <div className="auth-page">
       <div className="auth-card">
         <h2 className="auth-title">Login to OBSM</h2>
 
-        <form className="auth-form">
-          <input
-            type="email"
-            placeholder="Email"
-            className="auth-input"
-            required
-          />
+        <form className="auth-form" onSubmit={handleLogin}>
+          <input type="email" placeholder="Email" className="auth-input" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="auth-input"
-            required
-          />
+          <input type="password" placeholder="Password" className="auth-input" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
           <button className="auth-btn">Login</button>
         </form>
