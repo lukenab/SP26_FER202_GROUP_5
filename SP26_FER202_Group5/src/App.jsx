f68071e15b9cd3003f481481930d0859052589c9
ThanhDH <thanhdh.ce191422@gmail.com>
2026-03-10 07:44:10 +0700

    ThanhDh_ Add CartProvider and route/cart

--- SP26_FER202_Group5/src/App.jsx
+++ SP26_FER202_Group5/src/App.jsx
@@ -1,25 +1,32 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './components/Cart/CartGlobalState';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import BookListPage from './pages/BookListPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import BookDetailPage from './pages/BookDetailPage';
+import CartPage from './components/Cart/CartPage';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/books" element={<BookListPage />} />
-          <Route path="/books/:id"element={<BookDetailPage/>}></Route>
-        </Route>
-        {/* Auth pages */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/books" element={<BookListPage />} />
+            <Route path="/books/:id" element={<BookDetailPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+
+          {/* Auth pages */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
