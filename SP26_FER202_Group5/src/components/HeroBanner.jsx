f8c5abb49b669377bc4f7c74c76f314df3e2cb17
Luke Nab <anbinh2006ct@gmail.com>
2026-03-10 13:22:52 +0700

    css carousel

--- SP26_FER202_Group5/src/components/HeroBanner.jsx
+++ SP26_FER202_Group5/src/components/HeroBanner.jsx
@@ -2,7 +2,7 @@ import { Carousel } from "react-bootstrap";
 const HeroBanner = () => {
   const banners = [1,2,3,4,5];
   return (
-    <Carousel className="hero-banner shadow-sm">
+    <Carousel className="hero-banner shadow-sm mt-3">
       {banners.map((num) => (
         <Carousel.Item key={num}>
           <img  src={`/images/banner-${num}.webp`} className="d-block w-100" alt="banner" />
