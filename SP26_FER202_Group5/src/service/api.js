0efca52be11dba3d2500dce48198f569e1b5a9a9
trangttt <trangtttce191027@gmail.com>
2026-03-13 00:56:34 +0700

    TrangTTT_add deleteBook function for BookManagement

--- SP26_FER202_Group5/src/service/api.js
+++ SP26_FER202_Group5/src/service/api.js
@@ -1,18 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000';
 
 export const getAllBook = async () => {
-    const response = await axios.get(`${BASE_URL}/books`);
-    return response.data;
-}
+  const response = await axios.get(`${BASE_URL}/books`);
+  return response.data;
+};
 
-export const getAllCategories = async () =>{
-    const response = await axios.get(`${BASE_URL}/categories`);
-    return response.data;
-}
+export const getAllCategories = async () => {
+  const response = await axios.get(`${BASE_URL}/categories`);
+  return response.data;
+};
 
-export const getBookDetail = async (id) =>{
-    const response = await axios.get(`${BASE_URL}/books/${id}`);
+export const getBookDetail = async (id) => {
+  const response = await axios.get(`${BASE_URL}/books/${id}`);
+  return response.data;
+};
+
+export const deleteBook = async (id) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/books/${id}`);
     return response.data;
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
